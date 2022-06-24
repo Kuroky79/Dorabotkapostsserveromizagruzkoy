e0fe444546fc86a740d4bb3b7e0205a6619960b8
Truberov <timofey.bokach.d@gmail.com>
2022-06-24 03:00:12 +0300

    MyPosts

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <my-dialog :show="true">
+    <post-form
+        @create="createPost"
+    />
+    </my-dialog>
+    <post-list
+        :posts="posts"
+        @remove = removePost
+    />
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import PostForm from "./components/PostForm"
+import PostList from "@/components/PostList"
+import MyDialog from "@/components/UI/MyDialog";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    MyDialog,
+    PostForm, PostList
+  },
+  data(){
+    return {
+      posts: [
+        {id: 1, title: 'JavaScript' ,body: 'Описание поста'},
+        {id: 2, title: 'JavaScript 2' ,body: 'Описание поста 2'},
+        {id: 3, title: 'JavaScript 3' ,body: 'Описание поста 3'},
+        {id: 4, title: 'JavaScript 4' ,body: 'Описание поста 4'}
+      ],
+    }
+
+    },
+  methods:{
+    createPost(post){
+      this.posts.push(post);
+    },
+    removePost(post){
+      this.posts = this.posts.filter(p=> p.id !== post.id)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
+.app{
+  padding: 20px;
+}
+form {
+  display: flex;
+  flex-direction: column;
+}
+
 </style>
---
+++ src/components/UI/index.js
@@ -0,0 +1,6 @@
+import MyButton from "@/components/UI/MyButton";
+import myInput from "@/components/UI/MyInput";
+import myDialog from "@/components/UI/MyDialog";
+export default [
+    MyButton, myInput, myDialog
+]
